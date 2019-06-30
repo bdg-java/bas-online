6990f2a35e8e261071e749ddb10f8deb41927c76
araqs_sh <araqs.shahbazian@gmail.com>
2019-06-30 17:29:58 +0400

    Customer service

--- src/com/bdg/bas/ashahbazyan/service/account/AccountService.java
+++ src/com/bdg/bas/ashahbazyan/service/account/AccountService.java
@@ -1,9 +1,10 @@
-package com.bdg.bas.ashahbazyan.service;
+package com.bdg.bas.ashahbazyan.service.account;
 
 import com.bdg.bas.ashahbazyan.dto.AccountCreationRequest;
 import com.bdg.bas.ashahbazyan.entity.Account;
+import com.bdg.bas.ashahbazyan.service.BankAccountBaseService;
 
-public class AccountService implements  BankAccountBaseService<Account, AccountCreationRequest> {
+public class AccountService implements BankAccountBaseService<Account, AccountCreationRequest> {
 
     @Override
     public Account create(AccountCreationRequest request) {
