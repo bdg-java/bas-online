a5e62ccfebdb274a3ab684eea74f8aeb7704edd2
Nuard <nuard@atabekyan.com>
2019-06-30 17:37:44 +0400

    added CreditCard and Address services

---
+++ src/com/bdg/bas/natabekyan/service/address/AddressService.java
@@ -0,0 +1,13 @@
+package com.bdg.bas.natabekyan.service.address;
+
+import com.bdg.bas.natabekyan.dto.AddressCreationRequest;
+import com.bdg.bas.natabekyan.entity.Address;
+import com.bdg.bas.natabekyan.service.BankAccountBaseService;
+
+public class AddressService implements BankAccountBaseService<Address, AddressCreationRequest> {
+
+    @Override
+    public Address create(AddressCreationRequest request) {
+        return null;
+    }
+}
