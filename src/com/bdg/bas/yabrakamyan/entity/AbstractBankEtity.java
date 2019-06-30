9080c003c1718ac54eb9ee7e35bf165c34dab751
YAbrahamyan <yurabrahamyan44@gmail.com>
2019-06-30 15:33:07 +0400

    Bac live

--- src/com/bdg/bas/yabrakamyan/entity/AbstractBankEtity.java
+++ src/com/bdg/bas/yabrakamyan/entity/AbstractBankEtity.java
@@ -1,4 +1,11 @@
 package com.bdg.bas.yabrakamyan.entity;
 
-public class AbstractBankEtity {
+import java.time.LocalDate;
+
+public abstract class AbstractBankEtity {
+
+    protected int id;
+    protected LocalDate createdDate;
+    protected LocalDate updatedDate;
+    protected LocalDate deleted;
 }
