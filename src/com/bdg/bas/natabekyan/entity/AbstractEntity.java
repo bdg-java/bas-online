05bc44e95a04a2035fcf0d122fd8ca84d948b7ef
Nuard <nuard@atabekyan.com>
2019-06-30 15:24:34 +0400

    abstractentity

---
+++ src/com/bdg/bas/natabekyan/entity/AbstractEntity.java
@@ -0,0 +1,10 @@
+package com.bdg.bas.natabekyan.entity;
+
+import java.time.LocalDate;
+
+public class AbstractEntity {
+    private int id;
+    private LocalDate created;
+    private LocalDate deleted;
+    private LocalDate updated;
+}
