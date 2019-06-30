95829d91aaf42db230c334e731553328504be068
YAbrahamyan <yurabrahamyan44@gmail.com>
2019-06-30 17:37:27 +0400

    Bac live

---
+++ src/com/bdg/bas/yabrakamyan/Service/AddressService.java
@@ -0,0 +1,11 @@
+package com.bdg.bas.yabrakamyan.Service;
+
+import com.bdg.bas.yabrakamyan.dto.AddressCreationRequest;
+import com.bdg.bas.yabrakamyan.entity.Address;
+
+public class AddressService implements BankAccuontBaseService<Address, AddressCreationRequest> {
+    @Override
+    public Address create(AddressCreationRequest request) {
+        return null;
+    }
+}
---
+++ src/com/bdg/bas/yabrakamyan/Service/CreditCardService.java
@@ -0,0 +1,12 @@
+package com.bdg.bas.yabrakamyan.Service;
+
+import com.bdg.bas.yabrakamyan.dto.CreditCardCreationRequest;
+import com.bdg.bas.yabrakamyan.entity.CreditCard;
+
+public class CreditCardService  implements BankAccuontBaseService<CreditCard, CreditCardCreationRequest> {
+
+    @Override
+    public CreditCard create(CreditCardCreationRequest request) {
+        return null;
+    }
+}
