eda7451b38787a8dfc66c07988184e8a550d7621
araqs_sh <araqs.shahbazian@gmail.com>
2019-06-30 16:33:28 +0400

    Account

---
+++ src/com/bdg/bas/ashahbazyan/entity/Account.java
@@ -0,0 +1,7 @@
+package com.bdg.bas.ashahbazyan.entity;
+
+public class Account extends BankEntity {
+    private Customer customer;
+    private CreditCard creditCard;
+    private AccountBalance accountBalance;
+}
