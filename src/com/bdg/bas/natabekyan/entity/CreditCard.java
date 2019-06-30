9c2fec5544d24a98defeb59417975fadb7588b2c
Nuard <nuard@atabekyan.com>
2019-06-30 16:07:56 +0400

    added CreditCard

---
+++ src/com/bdg/bas/natabekyan/entity/CreditCard.java
@@ -0,0 +1,12 @@
+package com.bdg.bas.natabekyan.entity;
+
+import java.time.LocalDate;
+
+public class CreditCard extends AbstractEntity {
+
+    private int accountId;
+    private String cardNumber;
+    private LocalDate expDate;
+    private int cvv;
+
+}
