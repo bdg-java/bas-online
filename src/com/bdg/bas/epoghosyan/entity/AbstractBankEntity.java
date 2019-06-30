5b9e761872d499ff95abb7011358473332b60a10
VP95 <poghosyanvahe95@gmail.com>
2019-06-30 15:19:38 +0400

    epoghosyan entity push

---
+++ src/com/bdg/bas/epoghosyan/entity/AbstractBankEntity.java
@@ -0,0 +1,14 @@
+package com.bdg.bas.epoghosyan.entity;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class AbstractBankEntity {
+
+    int id;
+    LocalDate createdDate;
+    LocalDate updatedDate;
+    LocalDate deleted;
+
+
+}
