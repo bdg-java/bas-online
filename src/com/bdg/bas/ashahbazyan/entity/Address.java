fe4b58bfaa94266c32d6a64a0e57e4dcb2466603
araqs_sh <araqs.shahbazian@gmail.com>
2019-06-30 15:51:05 +0400

    Address

--- src/com/bdg/bas/ashahbazyan/entity/Address.java
+++ src/com/bdg/bas/ashahbazyan/entity/Address.java
@@ -1,4 +1,9 @@
 package com.bdg.bas.ashahbazyan.entity;
 
+import com.bdg.bas.ashahbazyan.common.Country;
+
 public class Address {
+    private String phoneNumber;
+    private String street;
+    private Country country;
 }
