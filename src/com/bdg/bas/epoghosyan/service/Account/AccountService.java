c8203bfc99fb9dd2222423e29af6ebf7ed615c93
VP95 <poghosyanvahe95@gmail.com>
2019-06-30 16:53:54 +0400

    epoghosyan services push

---
+++ src/com/bdg/bas/epoghosyan/service/Account/AccountService.java
@@ -0,0 +1,15 @@
+package com.bdg.bas.epoghosyan.service.Account;
+
+import com.bdg.bas.epoghosyan.dto.AccountCreationRequest;
+import com.bdg.bas.epoghosyan.entity.Account;
+import com.bdg.bas.epoghosyan.service.BankAccountBaseService;
+
+public class AccountService implements BankAccountBaseService<Account, AccountCreationRequest> {
+
+    @Override
+    public Account create(AccountCreationRequest request) {
+
+        return null;
+
+    }
+}
