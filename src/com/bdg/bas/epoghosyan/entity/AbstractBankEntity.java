eb8539c4997f070e20909c3879118832fbfc3068
VP95 <poghosyanvahe95@gmail.com>
2019-06-30 15:41:55 +0400

    epoghosyan customer push

--- src/com/bdg/bas/epoghosyan/entity/AbstractBankEntity.java
+++ src/com/bdg/bas/epoghosyan/entity/AbstractBankEntity.java
@@ -1,7 +1,6 @@
 package com.bdg.bas.epoghosyan.entity;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public abstract class AbstractBankEntity {
 
---
+++ src/com/bdg/bas/epoghosyan/entity/Customer.java
@@ -0,0 +1,14 @@
+package com.bdg.bas.epoghosyan.entity;
+
+public class Customer extends AbstractBankEntity {
+
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    public Customer(String firstName, String lastName, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+}
