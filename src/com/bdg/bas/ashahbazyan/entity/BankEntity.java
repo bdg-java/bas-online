4a8a4705f0fa6bb6fd4e6d371e1188f114e15851
araqs_sh <araqs.shahbazian@gmail.com>
2019-06-30 15:20:35 +0400

    Bank Entity

---
+++ src/com/bdg/bas/ashahbazyan/entity/BankEntity.java
@@ -0,0 +1,11 @@
+package com.bdg.bas.ashahbazyan.entity;
+
+import java.time.LocalDate;
+
+public abstract class BankEntity {
+
+    protected int id;
+    protected LocalDate created;
+    protected LocalDate updated;
+    protected LocalDate deleted;
+}
