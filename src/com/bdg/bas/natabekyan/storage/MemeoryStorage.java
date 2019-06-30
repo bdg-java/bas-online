8711efdee016c47fa1b23840e9894014450720c1
Nuard <nuard@atabekyan.com>
2019-06-30 18:08:37 +0400

    FileStorage and MemoryStorage

---
+++ src/com/bdg/bas/natabekyan/storage/MemeoryStorage.java
@@ -0,0 +1,21 @@
+package com.bdg.bas.natabekyan.storage;
+
+import com.bdg.bas.natabekyan.entity.AbstractEntity;
+
+public class MemeoryStorage implements Storage {
+
+    @Override
+    public AbstractEntity add(AbstractEntity entity) {
+        return null;
+    }
+
+    @Override
+    public AbstractEntity remove(int id) {
+        return null;
+    }
+
+    @Override
+    public AbstractEntity get(int id) {
+        return null;
+    }
+}
