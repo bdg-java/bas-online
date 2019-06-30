d6b6c8311f131e36d71d550b285770d6e20b957f
Nuard <nuard@atabekyan.com>
2019-06-30 15:58:09 +0400

    Address changes

--- src/com/bdg/bas/natabekyan/entity/Address.java
+++ src/com/bdg/bas/natabekyan/entity/Address.java
@@ -2,7 +2,8 @@ package com.bdg.bas.natabekyan.entity;
 
 import com.bdg.bas.natabekyan.common.Country;
 
-public class Address {
+public class Address extends AbstractEntity {
+
     private String phoneNumber;
     private String street;
     private Country country;
