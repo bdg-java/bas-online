f0ab0211ba3b1f5c2811bd7bf52823c3ae23d98f
Nuard <nuard@atabekyan.com>
2019-06-30 16:55:59 +0400

    changes in AccountService

--- src/com/bdg/bas/natabekyan/service/account/AccountService.java
+++ src/com/bdg/bas/natabekyan/service/account/AccountService.java
@@ -2,9 +2,9 @@ package com.bdg.bas.natabekyan.service.account;
 
 import com.bdg.bas.natabekyan.dto.AccountCreationRequest;
 import com.bdg.bas.natabekyan.entity.Account;
-import com.bdg.bas.natabekyan.service.BankAccuontBaseService;
+import com.bdg.bas.natabekyan.service.BankAccountBaseService;
 
-public class AccountService implements BankAccuontBaseService<Account, AccountCreationRequest> {
+public class AccountService implements BankAccountBaseService<Account, AccountCreationRequest> {
 
     @Override
     public Account create(AccountCreationRequest request) {
