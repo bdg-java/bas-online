9e2a42c47bd4075f1ebeec049f06149b8bc55a0b
Nuard <nuard@atabekyan.com>
2019-06-30 15:42:48 +0400

    Customer

---
+++ src/com/bdg/bas/natabekyan/Address.java
@@ -0,0 +1,4 @@
+package com.bdg.bas.natabekyan;
+
+public class Address {
+}
---
+++ src/com/bdg/bas/natabekyan/Customer.java
@@ -0,0 +1,10 @@
+package com.bdg.bas.natabekyan;
+
+import com.bdg.bas.natabekyan.entity.AbstractEntity;
+
+public class Customer extends AbstractEntity {
+
+    private String firstName;
+    private String lastName;
+    private Address address;
+}
