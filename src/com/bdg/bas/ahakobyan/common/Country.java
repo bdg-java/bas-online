79fbc669ea08d5cb869e5a0206094cff23bd8255
AbulHakobyan <abulhakobyan20@gmail.com>
2019-06-30 15:53:49 +0400

    abstract entity

---
+++ src/com/bdg/bas/ahakobyan/common/Country.java
@@ -0,0 +1,14 @@
+package com.bdg.bas.ahakobyan.common;
+
+public enum Country {
+
+    ARM("Armenia"), RU("Russia");
+
+
+    private String countryName;
+
+
+    Country(String countryName) {
+        this.countryName = countryName;
+    }
+}
