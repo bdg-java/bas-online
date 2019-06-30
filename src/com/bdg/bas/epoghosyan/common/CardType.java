5519a7253f7103eeb9693f652d00953982e2f2e7
VP95 <poghosyanvahe95@gmail.com>
2019-06-30 16:08:56 +0400

    Merge branch 'master' of C:\Users\pogho\IdeaProjects\bas-online with conflicts.

--- src/com/bdg/bas/epoghosyan/common/CardType.java
+++ src/com/bdg/bas/epoghosyan/common/CardType.java
@@ -1,7 +1,14 @@
 package com.bdg.bas.epoghosyan.common;
 
+<<<<<<< Updated upstream
 public enum  CardType {
         VISA,
         MASTERCARD,
         AMERICANEXPRESS
+=======
+public enum CardType {
+
+    VISA, MASTERCARD, ARCA, AMEX
+
+>>>>>>> Stashed changes
 }
--- src/com/bdg/bas/epoghosyan/entity/CreditCard.java
+++ src/com/bdg/bas/epoghosyan/entity/CreditCard.java
@@ -1,10 +1,14 @@
 package com.bdg.bas.epoghosyan.entity;
 
+<<<<<<< Updated upstream
 import com.bdg.bas.ashahbazyan.entity.BankEntity;
+=======
+>>>>>>> Stashed changes
 import com.bdg.bas.epoghosyan.common.CardType;
 
 import java.time.LocalDate;
 
+<<<<<<< Updated upstream
 public class CreditCard  extends BankEntity {
     private int accountId;
     private String cardNumber;
@@ -12,4 +16,61 @@ public class CreditCard  extends BankEntity {
     private int cvvCode;
     private CardType cardType;
 
+=======
+public class CreditCard extends AbstractBankEntity {
+
+    private int accountId;
+    private String cardNumber;
+    private LocalDate expDate;
+    private int CVV;
+    private CardType cardType;
+
+    public CreditCard(int accountId, String cardNumber, LocalDate expDate, int CVV, CardType cardType) {
+        this.accountId = accountId;
+        this.cardNumber = cardNumber;
+        this.expDate = expDate;
+        this.CVV = CVV;
+        this.cardType = cardType;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDate getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(LocalDate expDate) {
+        this.expDate = expDate;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public void setCVV(int CVV) {
+        this.CVV = CVV;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+>>>>>>> Stashed changes
 }
