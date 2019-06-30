305c61db8ad6d02bb50be60fde6d9bbde8b718a0
araqs_sh <araqs.shahbazian@gmail.com>
2019-06-30 16:48:31 +0400

    Account Service

---
+++ src/com/bdg/bas/ashahbazyan/service/AccountService.java
@@ -0,0 +1,11 @@
+package com.bdg.bas.ashahbazyan.service;
+
+import com.bdg.bas.ashahbazyan.entity.BankEntity;
+
+public class AccountService implements  BankAccountBaseService {
+
+    @Override
+    public BankEntity create(Object request) {
+        return null;
+    }
+}
